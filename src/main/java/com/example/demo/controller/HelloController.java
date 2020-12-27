8745e4d7f3559567fdc44f72d317a324420b32f5
soosungp33 <soosungp33@gmail.com>
2020-12-27 18:57:31 +0900

    MVC 알아보기

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -3,13 +3,20 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
     @GetMapping("hello")
     public String hello(Model model) {
-        model.addAttribute("data", "hello!");
+        model.addAttribute("data", "hello!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String hello(@RequestParam("name") String name, Model model) { // 외부에서 파라미터를 받음
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
