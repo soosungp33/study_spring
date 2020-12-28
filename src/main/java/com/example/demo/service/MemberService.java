5ba09121de46e0965a9dc829e598df95c69523a6
soosungp33 <soosungp33@gmail.com>
2020-12-28 17:54:02 +0900

    회원 서비스 만들어보기

---
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -0,0 +1,42 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.Member;
+import com.example.demo.repository.MemberRepository;
+import com.example.demo.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+        // 같은 이름의 회원은 x
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> { // Optional이여서 사용하는 방법(if != null과 같은 의미)
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원을 조회
+     */
+   public List<Member> findMembers() {
+       return memberRepository.findAll();
+   }
+
+   public Optional<Member> findOne(Long memberId) {
+       return memberRepository.findByID(memberId);
+   }
+}
