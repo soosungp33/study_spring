47ef91d6116314b5a9f52b01708c1e1687bd3fed
soosungp33 <soosungp33@gmail.com>
2021-01-10 18:30:19 +0900

    스프링 빈을 등록하는 두 번째 방법 -> 자바 코드를 통해 직접 스프링 빈으로 등록

--- src/main/java/com/example/demo/repository/MemoryMemberRepository.java
+++ src/main/java/com/example/demo/repository/MemoryMemberRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
-@Repository
+
 public class MemoryMemberRepository implements MemberRepository{
     // 동시성 문제 고려 x
     private static Map<Long, Member> store = new HashMap<>();
