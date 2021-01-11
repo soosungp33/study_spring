55017a6851a9166d0c3d7c1e5f576cf32a8e5a63
soosungp33 <soosungp33@gmail.com>
2021-01-11 15:31:09 +0900

    스프링 데이터 JPA

--- src/main/java/com/example/demo/SpringConfig.java
+++ src/main/java/com/example/demo/SpringConfig.java
@@ -1,35 +1,31 @@
 package com.example.demo;
 
-import com.example.demo.repository.JpaMemberRepository;
 import com.example.demo.repository.MemberRepository;
-import com.example.demo.repository.MemoryMemberRepository;
 import com.example.demo.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.Entity;
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
+    /*@Bean
     public MemberRepository memberRepository() {
 
-        return new JpaMemberRepository(em);
-    }
+        //return new JpaMemberRepository(em);
+
+    }*/
 }
